e3dfefa88a3bda2b1c53080e0dc7954b3e0b4e2f
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-03-10 10:57:35 +0530

    Time: 2 ms (94.05%) | Memory: 47.8 MB (34.58%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,20 +1,32 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        // int n=nums.length;
+        // int actualSum=0;
+        // Arrays.sort(nums);
+        // int total=n*(n+1)/2;
+        // for(int i=0;i<nums.length;i++){
+        //     actualSum+=nums[i];
+        // }
+        // int []ans=new int[2];
+        //     for(int i=1;i<n;i++){
+        //         if(nums[i]==nums[i-1]){
+        //             ans[0]=nums[i];
+        //             ans[1]=nums[i]+(total-actualSum);
+        //             }
+        //     }
+        //     return ans;
         int n=nums.length;
-        int actualSum=0;
-        Arrays.sort(nums);
-        int total=n*(n+1)/2;
-        for(int i=0;i<nums.length;i++){
-            actualSum+=nums[i];
+        int freq[]=new int[n+1];
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
         }
-        int []ans=new int[2];
-            for(int i=1;i<n;i++){
-                if(nums[i]==nums[i-1]){
-                    ans[0]=nums[i];
-                    ans[1]=nums[i]+(total-actualSum);
-                    }
-            }
-            return ans;
+        int r=-1;
+        int m=-1;
+        for(int i=1;i<=n;i++){
+            if(freq[i]>1) r=i;
+            else if(freq[i]==0) m=i;
+        }
+        return new int[]{r,m};
     }
  
 }
