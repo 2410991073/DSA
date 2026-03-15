56d2cb97da0a80acfb38a4cf02033159d96529b6
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-03-15 08:31:04 +0530

    Time: 1 ms (31.17%) | Memory: 43.1 MB (47.91%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-       s=s.trim();
-       String [] arr=s.split(" ");
-       String len=arr[arr.length-1];
-       return len.length(); 
+    //    s=s.trim();
+    //    String [] arr=s.split(" ");
+    //    String len=arr[arr.length-1];
+    //    return len.length(); 
+    s=s.trim();
+    String [] arr=s.split(" ");
+    String len=arr[arr.length-1];
+    return len.length();
+
     }
 }
