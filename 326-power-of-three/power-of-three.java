f17878da6e91b324e24c43832a89727505bef2fd
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-03-21 23:36:02 +0530

    Time: 8 ms (92.19%) | Memory: 45.7 MB (95.57%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,8 +1,15 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<=0) return false;
+        // if(n<=0) return false;
+        // while(n%3==0){
+        // n/=3;
+        // }
+        // return n==1;
+        if(n<=0){
+            return false;
+        }
         while(n%3==0){
-        n/=3;
+            n=n/3;
         }
         return n==1;
     }
