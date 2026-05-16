cc075de3fa3c6aa3fc14eb0f7dfd3b4cf9513514
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-05-16 20:26:48 +0530

    Time: 3 ms (87.79%) | Memory: 43.1 MB (72.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
+        // Stack<Character>st=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(ch=='(' || ch=='{' || ch=='['){
+        //         st.push(ch);
+        //     }
+        //     else if(ch==')' || ch=='}' || ch==']'){
+        //         if(st.isEmpty()){
+        //             return false;
+        //         }
+        //         char top=st.pop();
+        //         if((ch==')' && top!='(')||(ch=='}' && top!='{')||(ch==']' && top!='[')){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return st.isEmpty();
         Stack<Character>st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
@@ -11,7 +28,7 @@ class Solution {
                     return false;
                 }
                 char top=st.pop();
-                if((ch==')' && top!='(')||(ch=='}' && top!='{')||(ch==']' && top!='[')){
+                if((ch==')' && top!='(')||(ch==']' && top!='[') ||(ch=='}' && top!='{')){
                     return false;
                 }
             }
