a57b4c7de249b2fda9878fcec008df8be19276d5
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-05-17 11:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (91.96%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -21,16 +21,16 @@ class Solution {
         //     }
         // }
         // return head;
-        if(head==null) return null;
-        ListNode temp=head;
-        while(temp!=null && temp.next!=null){
-            if(temp.val==temp.next.val){
-                temp.next=temp.next.next;
-            }
-            else{
-                temp=temp.next;
-            }
+      if(head==null) return null;
+      ListNode temp=head;
+      while(temp!=null && temp.next!=null){
+        if(temp.val==temp.next.val){
+            temp.next=temp.next.next;
         }
-        return head;
+        else{
+            temp=temp.next;
+        }
+      }
+      return head;
     }
 }
