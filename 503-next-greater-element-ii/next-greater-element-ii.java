891dfbdf40ebe23425dd2837ad472caf4190b111
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-05-27 14:57:16 +0530

    Time: 14 ms (62.43%) | Memory: 48.9 MB (6.02%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        // int n=nums.length;
         // Stack<Integer>st=new Stack<>();
+        // int n=nums.length;
         // int nge[]=new int[n];
         // for(int i=2*n-1;i>=0;i--){
         //     int num=nums[i%n];
@@ -9,7 +9,7 @@ class Solution {
         //         st.pop();
         //     }
         //     if(i<n){
-        //         nge[i]=st.isEmpty() ?-1:st.peek();
+        //         nge[i]=st.isEmpty()?-1:st.peek();
         //     }
         //     st.push(num);
         // }
@@ -23,7 +23,7 @@ class Solution {
                 st.pop();
             }
             if(i<n){
-                nge[i]=st.isEmpty()?-1:st.peek();
+                nge[i]=st.isEmpty() ?-1:st.peek();
             }
             st.push(num);
         }
