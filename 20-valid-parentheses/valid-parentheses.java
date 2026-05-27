0d63b8c34751fe688479da316a91296cf684d643
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-05-27 13:00:10 +0530

    Time: 4 ms (39.62%) | Memory: 43.5 MB (15.34%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,27 +3,28 @@ class Solution {
         // Stack<Character>st=new Stack<>();
         // for(int i=0;i<s.length();i++){
         //     char ch=s.charAt(i);
-        //     if(ch=='(' || ch=='{' || ch=='['){
+        //     if(ch=='(' || ch=='[' || ch=='{'){
         //         st.push(ch);
         //     }
-        //     else if(ch==')' || ch=='}' || ch==']'){
+        //     else if(ch==')' || ch==']' || ch=='}'){
         //         if(st.isEmpty()){
         //             return false;
         //         }
         //         char top=st.pop();
-        //         if((ch==')' && top!='(')||(ch=='}' && top!='{')||(ch==']' && top!='[')){
+        //         if((ch==')' && top!='(') ||(ch==']' && top!='[') || (ch=='}' && top!='{')){
         //             return false;
         //         }
         //     }
         // }
         // return st.isEmpty();
+
         Stack<Character>st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='(' || ch=='[' || ch=='{'){
+            if(ch=='(' || ch=='{' || ch=='['){
                 st.push(ch);
             }
-            else if(ch==')' || ch==']' || ch=='}'){
+            else if(ch==')' || ch=='}' || ch==']'){
                 if(st.isEmpty()){
                     return false;
                 }
