3498f884877ebb5f0c09d53fc71db85272f7c765
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-05-28 16:28:11 +0530

    Time: 3 ms (88.09%) | Memory: 43 MB (84.17%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,19 +3,18 @@ class Solution {
         Stack<Character>st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='('|| ch=='['|| ch=='{'){
+            if(ch=='('|| ch=='{'|| ch=='['){
                 st.push(ch);
             }
-            else if(ch==')' || ch=='}' || ch==']'){
+             else if(ch==')' || ch==']' || ch=='}'){
                 if(st.isEmpty()){
                     return false;
-                
-            }
-            char top=st.pop();
-            if((ch==')' && top!='(') ||(ch==']' && top!='[')||(ch=='}' && top!='{')){
-                return false;
-            }
-        }
+                }
+                char top=st.pop();
+                if((ch==')' && top!='(')||(ch==']' && top!='[')||(ch=='}' && top!='{')){
+                    return false;
+                }
+             }
         }
         return st.isEmpty();
     }
