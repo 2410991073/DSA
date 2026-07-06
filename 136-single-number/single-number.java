148b773d320fad2a95a4cf4dba91474262d9cd00
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-06 23:36:01 +0530

    Time: 1 ms (99.97%) | Memory: 47.1 MB (27.92%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor=0;
-        for(int ele:nums){
-            xor=xor^ele;
-        }
-        return xor;
+      int ans=0;
+      for(int ele:nums){
+        ans=ans^ele;
+      }
+      return ans;
     }
 }
