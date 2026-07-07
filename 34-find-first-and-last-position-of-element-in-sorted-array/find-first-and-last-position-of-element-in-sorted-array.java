78b5378aa14ea50ab9ff4752ce569700883bab93
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-07 13:03:06 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (70.47%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,42 +3,49 @@ class Solution {
         int first=leftside(nums,target);
         int last=rightside(nums,target);
         return new int[]{first,last};
-    }
-    private static int leftside(int[]nums,int target){
-        int low=0,high=nums.length-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return ans;
-    }
-    private static int rightside(int nums[],int target){
-        int low=0,high=nums.length-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return ans;
 
     }
+    private static int leftside(int[]nums,int target){
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+               
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+
+        }
+        return ans;
+    }
+    private static int rightside(int[]nums,int target){
+        int low=0;
+        int high=nums.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target){
+              low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+
+        }
+        return ans;
+    }
+
 }
