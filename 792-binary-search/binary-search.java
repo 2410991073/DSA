9dcb1f6a1e3a000363bc089890f29d2e5e94f42d
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-07 11:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (68.90%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int helper(int [] nums,int target,int lo,int hi){
+        if(lo>hi) return -1;
+        int mid=lo+(hi-lo)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[mid]<target){
+            return helper(nums,target,mid+1,hi);
+        }
+        return helper(nums,target,lo,mid-1);
+    }
+     public int search(int[] nums, int target) {
+        return helper(nums, target, 0, nums.length - 1);
+    }
+}
