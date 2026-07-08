5b87c0de0afdc13fd2beeafefb3bbc777fc7b0e7
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-08 18:57:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (52.87%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -10,7 +10,7 @@ class Solution {
         }
         for(int i=0;i<n;i++){
             int left=0,right=n-1;
-            while(left<=right){
+            while(left<right){
                 int temp=matrix[i][left];
                 matrix[i][left]=matrix[i][right];
                 matrix[i][right]=temp;
