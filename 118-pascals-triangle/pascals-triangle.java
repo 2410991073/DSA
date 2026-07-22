82128b9d9a87ecdc5578fa69ce27b7dd2f9876e7
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-22 15:38:41 +0530

    Time: 1 ms (94.31%) | Memory: 43.5 MB (33.65%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,19 +1,32 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>>ans=new ArrayList<>();
-        for(int i=0;i<numRows;i++){
-            List<Integer>path=new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==i){
-                    path.add(1);
-                }
-                else{
-                    int val=ans.get(i-1).get(j-1)+ans.get(i-1) .get(j);
-                    path.add(val);
-                }
+    //     List<List<Integer>>ans=new ArrayList<>();
+    //     for(int i=0;i<numRows;i++){
+    //         List<Integer>row=new ArrayList<>();
+    //         for(int j=0;j<=i;j++){
+
+    //         if(j==0 || j==i)
+    //             row.add(1);
+            
+    //                     else{
+    //             row.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
+    //         }
+    //         }
+    //         ans.add(row);
+    //     }
+    //    return ans;
+    List<List<Integer>>ans=new ArrayList<>();
+    for(int i=0;i<numRows;i++){
+        List<Integer>row=new ArrayList<>();
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i)
+            row.add(1);
+            else{
+                row.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
             }
-            ans.add(path);
         }
-        return ans;
+        ans.add(row);
+    }
+    return ans;
     }
 }
