2feee31eda1762f746b0b995bc38e25405a27d37
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-22 19:04:56 +0530

    Time: 1 ms (24.06%) | Memory: 43.7 MB (11.05%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,17 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // int n=nums.length;
-        // for(int i=0;i<n;i++){
-        //     int minidx=i;
-        //     for(int j=i+1;j<n;j++){
-        //         if(nums[j]<nums[minidx]){
-        //             minidx=j;
-        //         }
-        //     }
-        //     int temp=nums[minidx];
-        //     nums[minidx]=nums[i];
-        //     nums[i]=temp; 
-        // }
+    // int n=nums.length;
+    // for(int i=0;i<nums.length;i++){
+    //     int minindex=i;
+    //     for(int j=i+1;j<n;j++){
+    //         if(nums[j]<nums[minindex]){
+    //             minindex=j;
+    //         }
+    //     }
+    //     int temp=nums[minindex];
+    //     nums[minindex]=nums[i];
+    //     nums[i]=temp;
     // }
     int n=nums.length;
     for(int i=0;i<nums.length;i++){
@@ -24,7 +23,6 @@ class Solution {
         int temp=nums[minindex];
         nums[minindex]=nums[i];
         nums[i]=temp;
-    }
-        
+    }  
     }
 }
