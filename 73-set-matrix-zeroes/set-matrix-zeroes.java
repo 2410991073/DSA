6e9160c013e16225e429f01a37d9ff494900508d
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-23 13:08:12 +0530

    Time: 1 ms (98.16%) | Memory: 47.8 MB (25.48%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,18 +2,18 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        boolean[]row=new boolean[m];
+        boolean[]rows=new boolean[m];
         boolean[]cols=new boolean[n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=true;
+                    rows[i]=true;
                     cols[j]=true;
                 }
             }
         }
         for(int i=0;i<m;i++){
-            if(row[i]){
+            if(rows[i]){
                 for(int j=0;j<n;j++){
                     matrix[i][j]=0;
                 }
