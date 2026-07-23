71d1340132b41d93b1fd3322de72e8b4fdaa091f
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-07-23 20:48:10 +0530

    Time: 8 ms (91.99%) | Memory: 49.3 MB (14.56%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,19 +1,6 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
-        // List<int[]>ans=new ArrayList<>();
-        // ans.add(intervals[0]);
-        // for(int i=1;i<intervals.length;i++){
-        //     int[]last=ans.get(ans.size()-1);
-        //     if(intervals[i][0]<=last[1]){
-        //         last[1]=Math.max(last[1],intervals[i][1]);
-        //     }
-        //     else{
-        //         ans.add(intervals[i]);
-        //     }
-        // }
-        // return ans.toArray(new int[ans.size()][]);
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
         List<int[]>ans=new ArrayList<>();
         ans.add(intervals[0]);
         for(int i=1;i<intervals.length;i++){
@@ -24,7 +11,6 @@ class Solution {
             else{
                 ans.add(intervals[i]);
             }
-            
         }
         return ans.toArray(new int[ans.size()][]);
     }
