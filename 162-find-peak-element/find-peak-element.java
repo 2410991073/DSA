0f07b554922deda053fabea2b1313ad399e5eeed
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-08 23:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (38.07%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,18 +1,6 @@
 class Solution {
     public int findPeakElement(int[] arr) {
-        // int start=0;
-        // int end=arr.length-1;
-        // while(start<end){
-        //     int mid=start+(end-start)/2;
-        //     if(arr[mid]>arr[mid+1]){
-        //         end=mid;
-        //     }
-        //     else{
-        //         start=mid+1;
-        //     }
-        // }
-        // return start;
-        int low=0;
+         int low=0;
         int high=arr.length-1;
         while(low<high){
             int mid=low+(high-low)/2;
