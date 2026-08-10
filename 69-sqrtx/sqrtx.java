3292eca7249c1745787e99406f666da1b07e6fb4
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-10 18:57:59 +0530

    Time: 1 ms (97.86%) | Memory: 42.7 MB (43.45%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,7 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
         // if(x==0 || x==1) return x;
-        // int low=0,high=x;
+        // int low=0;
+        // int high=x;
         // int ans=0;
         // while(low<=high){
         //     int mid=low+(high-low)/2;
