0dd70e253e9a0dd6fb23774d5ef2129aec003704
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-11 21:03:20 +0530

    Time: 2 ms (99.76%) | Memory: 43.6 MB (54.51%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,9 +1,9 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         StringBuilder sb=new StringBuilder();
-        int count=0;
+        int count =0;
         for(char ch:s.toCharArray()){
-            if(ch=='('){
+             if(ch=='('){
                 if(count>0){
                     sb.append(ch);
                 }
