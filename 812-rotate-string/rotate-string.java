091417062602fe766a6d381e27d227668093a0a2
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-12 12:37:57 +0530

    Time: 2 ms (56.03%) | Memory: 43 MB (83.40%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
-            return false;
-        }
+        if(s.length()!=goal.length()) return false;
         String doubled=s+s;
-        if(doubled.contains(goal)){
-            return true;
-        }
+        if(doubled.contains(goal)) return true;
         return false;
     }
 }
