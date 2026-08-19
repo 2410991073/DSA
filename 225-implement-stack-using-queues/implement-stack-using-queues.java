130fb8f49d8792d814da137c590f5fb57d77dee5
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-19 10:57:21 +0530

    Time: 1 ms (82.26%) | Memory: 43 MB (18.30%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -4,20 +4,22 @@ class MyStack {
         q=new LinkedList<>();
     }
     
-    public void push(int x){
+    public void push(int x) {
         q.add(x);
-        int size=q.size();
-        for(int i=0;i<size-1;i++){
+        for(int i=0;i<q.size()-1;i++){
             q.add(q.remove());
         }
     }
-    public int pop(){
+    
+    public int pop() {
         return q.remove();
     }
-    public int top(){
+    
+    public int top() {
         return q.peek();
     }
-    public boolean empty(){
+    
+    public boolean empty() {
         return q.isEmpty();
     }
 }
