4e71dc89158f0e7dfd4e14afae12ac78b1d1cde6
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-19 20:41:22 +0530

    Time: 2 ms (19.29%) | Memory: 46.7 MB (62.57%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,44 +15,24 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        // List<List<Integer>>ans=new ArrayList<>();
-        // Queue<TreeNode>q=new LinkedList<>();
-        // if(root==null) return ans;
-        // q.add(root);
-        // while(q.size()>0){
-        //     int size=q.size();
-        //     List<Integer>level=new ArrayList<>();
-        //     for(int i=0;i<size;i++){
-        //         TreeNode temp=q.peek();
-        //         q.remove();
-        //         level.add(temp.val);
-        //         if(temp.left!=null) q.add(temp.left);
-        //         if(temp.right!=null) q.add(temp.right);
-        //     }
-        //     ans.add(level);
-        // }
-        // return ans;
         List<List<Integer>>ans=new ArrayList<>();
         Queue<TreeNode>q=new LinkedList<>();
         if(root==null) return ans;
         q.add(root);
-        while(q.size()>0){
+        
+        while(!q.isEmpty()){
             int size=q.size();
             List<Integer>level=new ArrayList<>();
             for(int i=0;i<size;i++){
+                TreeNode temp=q.peek();
+                q.remove();
 
-            
-            TreeNode temp=q.peek();
-            q.remove();
-            level.add(temp.val);
-            if(temp.left!=null) q.add(temp.left);
-            if(temp.right!=null) q.add(temp.right);
-
-        }
-
-        ans.add(level);
+                level.add(temp.val);
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
+            }
+            ans.add(level);
         }
         return ans;
-
     }
 }
