e308982cedbd61a9009aee73963d08f408ea2082
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-24 11:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (55.77%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,28 +1,55 @@
 class Solution {
     public int trap(int[] height) {
+        // int left=0;
+        // int right=height.length-1;
+        // int leftmax=0;
+        // int rightmax=0;
+        // int water=0;
+        // while(left<right){
+        //    if(height[left]<height[right]){
+        //     if(height[left]>=leftmax){
+        //         leftmax=height[left];
+        //     }else{
+        //         water+=leftmax-height[left];
+        //     }
+        //     left++;
+        //    } 
+        //    else{
+        //     if(height[right]>=rightmax){
+        //         rightmax=height[right];
+        //     }
+        //     else{
+        //         water+=rightmax-height[right];
+        //     }
+        //     right--;
+        //    }
+        // }
+        // return water;
         int left=0;
         int right=height.length-1;
         int leftmax=0;
         int rightmax=0;
         int water=0;
         while(left<right){
-           if(height[left]<height[right]){
-            if(height[left]>=leftmax){
-                leftmax=height[left];
-            }else{
-                water+=leftmax-height[left];
-            }
-            left++;
-           } 
-           else{
-            if(height[right]>=rightmax){
-                rightmax=height[right];
+            if(height[left]<height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }
+                else{
+                    water+=leftmax-height[left];
+                }
+                left++;
             }
             else{
-                water+=rightmax-height[right];
+                if(height[right]>rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    water+=rightmax-height[right];
+                }
+                right--;
             }
-            right--;
-           }
+
         }
         return water;
     }
