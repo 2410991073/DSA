c628a8e20fc0241e704ab2ce063d4db897743023
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-08-31 19:18:11 +0530

    Time: 502 ms (93.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+-- SELECT  product_id FROM  Products where low_fats='Y' AND recyclable='Y'
+
+
+select product_id from products where low_fats='Y' and recyclable='Y'
