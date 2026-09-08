0c971951376adc646ef820aeb43c5c5ef9e35749
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-08 13:11:19 +0530

    Time: 2 ms (97.73%) | Memory: 72.4 MB (49.41%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -4,27 +4,25 @@ class Solution {
         for(int bill:bills){
             if(bill==5) five++;
             else if(bill==10){
-                if(five==0){
-                    return false;
-                }
+            if(five==0){
+                return false;
+            }
+            five--;
+            ten++;
+            }
+            else{
+                if(ten>0 && five>0){
+                    ten--;
                     five--;
-                    ten++;
-
+                }
+                else if(five>=3){
+                    five-=3;
                 }
                 else{
-                    if(ten>0 && five>0){
-                        ten--;
-                        five--;
-                    }
-                    else if(five>=3){
-                        five-=3;
-                    }
-                    else{
-                        return false;
-                    }
+                    return false;
                 }
+            }
             
-
         }
         return true;
     }
