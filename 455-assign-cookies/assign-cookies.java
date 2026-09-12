08a054ec7198789299030faa8036526609cb5fc0
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-12 10:55:01 +0530

    Time: 15 ms (59.97%) | Memory: 51.5 MB (55.65%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -2,8 +2,7 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int child=0;
-        int cookie=0;
+        int child=0,cookie=0;
         while(child<g.length && cookie<s.length){
             if(s[cookie]>=g[child]){
                 child++;
