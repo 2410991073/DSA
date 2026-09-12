1492598ac77bc7586bcce1ca9c644e47f18fb8ce
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-12 11:02:53 +0530

    Time: 33 ms (11.98%) | Memory: 90.7 MB (46.67%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,22 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-
         Arrays.sort(players);
         Arrays.sort(trainers);
-
-        int player = 0;
-        int trainer = 0;
-
-        while (player < players.length && trainer < trainers.length) {
-
-            if (players[player] <= trainers[trainer]) {
+        int player=0,trainer=0;
+        while(player<players.length && trainer<trainers.length){
+            if(trainers[trainer]>=players[player]){
                 player++;
             }
             trainer++;
         }
-
         return player;
     }
 }
