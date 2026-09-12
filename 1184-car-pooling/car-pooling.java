77ff53cc1e5b2c773976d1496634c2075bfb99d7
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-12 12:28:36 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (77.75%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-         int[]stops=new int[1001];
-         for(int[]trip:trips){
+        int stop[]=new int[1001];
+        for(int[]trip:trips){
             int passenger=trip[0];
             int from=trip[1];
             int to=trip[2];
-            stops[from]+=passenger;
-            stops[to]-=passenger;
-         }
-         int current=0;
-         for(int i=0;i<=1000;i++){
-            current+=stops[i];
+            stop[from]+=passenger;
+            stop[to]-=passenger;
+        }
+        int current=0;
+        for(int i=0;i<=1000;i++){
+            current+=stop[i];
             if(current>capacity){
                 return false;
             }
-         }
-         return true;
+        }return true;
     }
 }
