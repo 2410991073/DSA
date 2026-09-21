aa89acf2dcdd8003952c74ca27f8f12e8b4be0b8
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-21 11:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (38.01%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-        int dp[]=new int[n];
+        int[]dp=new int[n];
         if(n==1) return nums[0];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
