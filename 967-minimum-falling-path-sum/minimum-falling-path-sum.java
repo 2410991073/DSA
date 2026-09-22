5dfd81ee04a0b5d8801677601bbb474dccd6cfc6
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-22 13:05:39 +0530

    Time: 7 ms (48.62%) | Memory: 47.4 MB (31.48%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -0,0 +1,42 @@
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m][n];
+
+        // First row
+        for(int j = 0; j < n; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+
+        // Start from second row
+        for(int i = 1; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                int min = dp[i-1][j];
+
+                if(j > 0) {
+                    min = Math.min(min, dp[i-1][j-1]);
+                }
+
+                if(j < n-1) {
+                    min = Math.min(min, dp[i-1][j+1]);
+                }
+
+                dp[i][j] = matrix[i][j] + min;
+            }
+        }
+
+        int ans = dp[m-1][0];
+
+        for(int j = 1; j < n; j++) {
+            ans = Math.min(ans, dp[m-1][j]);
+        }
+
+        return ans;
+    }
+}
+
