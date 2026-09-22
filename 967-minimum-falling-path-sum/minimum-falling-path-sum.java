f86fa5ed7606b2d0883d9c5cc93a968e4935083b
Devyani Salwan <devyani1073.becse24@chitkara.edu.in>
2026-09-22 13:06:23 +0530

    Time: 7 ms (48.62%) | Memory: 47.4 MB (31.48%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -4,15 +4,11 @@ class Solution {
 
         int m = matrix.length;
         int n = matrix[0].length;
-
-        int[][] dp = new int[m][n];
-
-        // First row
+        int[][]dp=new int[m][n];
         for(int j = 0; j < n; j++) {
             dp[0][j] = matrix[0][j];
         }
 
-        // Start from second row
         for(int i = 1; i < m; i++) {
             for(int j = 0; j < n; j++) {
 
